33ec98b61ef8736396715a3ec594994c0e49883b
khanhnd <khanhnd@sotanext.com>
2024-08-08 17:29:02 +0700

    save

--- src/environment.ts
+++ src/environment.ts
@@ -4,8 +4,5 @@ const env = import.meta.env;
 const Environment = {
   SERVER_REST: env.VITE_SERVER_REST,
   SERVER_SOCKET: env.VITE_SERVER_SOCKET,
-  PARTICLE_PROJECT_ID: env.VITE_PARTICLE_PROJECT_ID,
-  PARTICLE_CLIENT_KEY: env.VITE_PARTICLE_CLIENT_KEY,
-  PARTICLE_APP_ID: env.VITE_PARTICLE_APP_ID,
 };
 export default Environment;
